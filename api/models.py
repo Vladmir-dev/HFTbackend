7e24d09d40156db781d057577445cada86bb7b1a
Vladmir-dev <tempestpius70@gmail.com>
2021-10-22 11:01:15 +0300

    added drop downs

--- api/models.py
+++ api/models.py
@@ -1,26 +1,46 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-# class User(models.Model):
-#     username = models.CharField(max_length=150)
-#     email = models.EmailField(max_length=200, unique=True)
-#     password = models.CharField(max_length=200)
-#
-#     objects =  UserManager()
-#
-#     def __str__(self):
-#         return self.username
+ACCOUNTS = (
+    ('paper_account', 'Paper Account'),
+    ('live_account', 'Live Account'),
+)
 
+COUNTRIES = (
+    ('uganda', 'Uganda'),
+    ('kenya', 'Kenya'),
+    ('tanzania', 'Tanzania'),
+    ('rwanda', 'Rwanda'),
+    ('burundi', 'Burundi'),
+    ('south_sudan', 'South Sudan'),
+)
+
+CITIES = (
+    ('kampala', 'Kampala'),
+    ('Mbarara', 'Mbarara'),
+    ('Gulu', 'Gulu'),
+    ('dar_es_salaam', '	Dar es Salaam'),
+    ('mwanza', 'Mwanza'),
+    ('arusha', 'Arusha'),
+    ('nairobi', 'Nairobi'),
+    ('kisumu', 'Kisumu'),
+    ('mombasa', 'Mombasa'),
+    ('kigali', 'Kigali'),
+    ('butare', 'Butare'),
+    ('gitarama', 'Gitarama'),
+    ('bujumbura', 'Bujumbura'),
+    ('gitega', 'Gitega'),
+    ('muyinga', 'Muyinga'),
+)
 
 class Profile(models.Model):
-    account_type = models.CharField(max_length=100)
+    account_type = models.CharField(max_length=100, choices=ACCOUNTS, blank=True)
     mobile_number = models.IntegerField(unique=True)
-    country_of_residence = models.CharField(max_length=100)
-    city = models.CharField(max_length=100)
+    country_of_residence = models.CharField(max_length=100, choices=COUNTRIES, blank=True)
+    city = models.CharField(max_length=100, choices=CITIES, blank=True)
     zip_code = models.IntegerField()
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, null=True
+        User, related_name="profiles", on_delete=models.CASCADE, null=True
     )
 
     def __str__(self):
