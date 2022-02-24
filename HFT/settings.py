d7121068a13a7dcc7f1b6d4dff713b3f6835b304
Vladmir-dev <tempestpius70@gmail.com>
2022-02-24 21:55:33 +0300

    added cors whitelis

--- HFT/settings.py
+++ HFT/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -136,6 +137,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:3001",
+]
 
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
